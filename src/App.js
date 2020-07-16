dac89d513bcab3706e2c027979d40562b09e3c7f
DDA209 <didier.jeux1@gmail.com>
2020-07-16 22:51:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Counter from './components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {count: 1};
+
+  /*incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+    console.log("pouet", this.state);
+  };
+  decrementCount = () => {
+    this.state.count === 1 ? this.setState({
+      count: 1
+    }) :
+    this.setState({
+      count: this.state.count - 1
+    });
+    console.log("tweip", this.state);
+  };*/
+
+  constructor(props) {
+    super(props);
+    this.incrementCount = this.incrementCount.bind(this);
+    this.decrementCount = this.decrementCount.bind(this);
+  };   
+
+  incrementCount() {
+
+    this.setState({
+      count: this.state.count + 1
+    });
+    console.log("pouet", this.state);
+  };
+
+  decrementCount() {
+    this.state.count === 1 ? this.setState({
+      count: 1
+    }) :
+    this.setState({
+      count: this.state.count - 1
+    });
+    console.log("tweip", this.state);
+  };
+
+  render(){
+    return(
+      <div>
+          <h1>Counter</h1>
+          <Counter
+            incrementFn = {() => this.incrementCount()}
+            decrementFn = {() => this.decrementCount()}>
+            {this.state.count}
+          </Counter>
+          
+          {/* <h2>{this.state.count}</h2>
+          <button onClick={() => this.incrementCount()}>+</button>
+          <button onClick={() => this.decrementCount()}>-</button> */}
+      </div>
+    );
+  }
 }
 
 export default App;
